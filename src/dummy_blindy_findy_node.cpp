918b7d6b10fd6bab7c4fd7972e11b79402618d3b
byteofsoren <r00tr4t@gmail.com>
2018-05-09 18:05:53 +0200

    Fixed a loop sleap and removed old text
    Signed-off-by: byteofsoren <r00tr4t@gmail.com>

--- src/dummy_blindy_findy_node.cpp
+++ src/dummy_blindy_findy_node.cpp
@@ -1,123 +1,7 @@
 
 #include <ros/ros.h>
-
 #include "blindy_findy/distances.h"
-
-
-/*int ReturnEndOfFloor(float line[], int windowSize, int height)
-  {
-
-     double slope = 0;
-     int index = windowSize;
-     while (slope <= 0 && index >= 1)
-     {
-       index--;
-       {
-         slope = line[(index + 1) * (height - 1) / windowSize] - line[(index * height) / windowSize];
-       }
-     }
-     index = (index + 1) * (height - 1) / windowSize;
-     return index;
-   }
-
-float ReturnSmallestDistance(float line[], int index)
-  {
-     float shortest = 666;
-     for (int i = index; i >= 50; i--)
-     {
-        if (line[i] < shortest && line[i] !=0)
-           shortest = line[i];
-     }
-     return shortest;
-   }*/
-
-/*void republish()
-    {
-      //VGA setting in ZED parameters
-      int height = 376;
-      int width = 672;
-  
-      float rightline[height];
-      float midline[height];
-      float leftline[height];
-      int numberOfLines = 3;
-      //float lines[numberOfLines][height];
-      bool firstFrame = true;
-      float distVal[3];
-
-      cv_bridge::CvImagePtr localptr = cv_ptr;
-    
-      //-------put code here-----------------------------------------
-      float depthMap[height][width];
-      for(int n=0;n<height;n++)
-      {
-        for(int m=0;m<width;m++)
-        {
-          cv::Scalar intensity = localptr->image.at<float>(n,m);
-          depthMap[n][m]= intensity.val[0];
-          if(m==(height/4))
-            rightline[n] = depthMap[n][m];
-          if(m==(2*height/4))
-            midline[n] = depthMap[n][m];
-          if(m==(3*height/4))
-            leftline[n] = depthMap[n][m];
-        }
-      }
-
-      cv::line(localptr->image, cv::Point(width/4,0),cv::Point(width/4,height),
-   cv::Scalar( 0, 0, 0 ),
-   10,
-   cv::LINE_8);
-      cv::line(localptr->image, cv::Point(2*width/4,0),cv::Point(2*width/4,height),
-   cv::Scalar( 0, 0, 0 ),
-   10,
-   cv::LINE_8);
-      cv::line(localptr->image, cv::Point(3*width/4,0),cv::Point(3*width/4,height),
-   cv::Scalar( 0, 0, 0 ),
-   10,
-   cv::LINE_8);
-
-      distVal[0] = ReturnSmallestDistance(midline, ReturnEndOfFloor(midline, 10, height));
-      distVal[1] = ReturnSmallestDistance(midline, ReturnEndOfFloor(midline, 10, height));
-      distVal[2] = ReturnSmallestDistance(midline, ReturnEndOfFloor(midline, 10, height));
-      printf("VAL: %f\n", distVal[0]);
-      //-------------------------------------------------------------
-      //blindy_findy::distances dmsg;
-        //dmsg.distances.clear();
-      //dmsg.distances.x = distVal[0];
-        //dmsg.distances.y = distVal[1];
-        //dmsg.distances.z = distVal[2];
-      //ROS_INFO("%f", dmsg.distances.x);
-      //pub->publish(dmsg);
-  }
-
-
-void Cbfunc(const sensor_msgs::ImageConstPtr& msg)
-{
-  int height = 376;
-  int width = 672;
-
-  float rightline[height];
-  float midline[height];
-  float leftline[height];
-  
-  int numberOfLines = 3;
-
-    
-  //-------put code here-----------------------------------------
-
-  distVal[0] = ReturnSmallestDistance(leftline, ReturnEndOfFloor(leftline, 10, height));
-  distVal[1] = ReturnSmallestDistance(midline, ReturnEndOfFloor(midline, 10, height));
-  distVal[2] = ReturnSmallestDistance(rightline, ReturnEndOfFloor(rightline, 10, height));
-  //printf("VAL: %f\n", distVal[0]);
-  //put data in msg and publish-----------------------------------------------------
-  blindy_findy::distances dmsg;
-  dmsg.distL = distVal[0];
-  dmsg.distM = distVal[1];
-  dmsg.distR = distVal[2];
-  //ROS_INFO("L: %f\nM: %f\nR: %f\n", dmsg.distL, dmsg.distM, dmsg.distR);
-  pub->publish(dmsg);
-}*/
+#include <unistd.h>
 
 
 int main(int argc, char** argv)
@@ -140,8 +24,8 @@ int main(int argc, char** argv)
     dmsg.distR = distVal[2];
     ROS_INFO("L: %f\nM: %f\nR: %f\n", dmsg.distL, dmsg.distM, dmsg.distR);
     pub.publish(dmsg);
-
     ros::spinOnce();
+    loop_rate.sleep();
   }
     return 0;
 }
